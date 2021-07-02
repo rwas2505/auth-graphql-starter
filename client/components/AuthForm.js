1006237066b138eb479bd729cb1786c0cce74cfe
Ryan Waszak <rwaszak@tesla.com>
2021-07-02 15:44:45 -0700

    100 pass function to call login mutation as prop from LoginForm to AuthForm, then when submitting form in AuthForm, have it call the method from the prop which will actually execute the mutation in LoginForm

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -7,20 +7,29 @@ class AuthForm extends Component {
         this.state = { email: '', password: ''};
     }
 
+    onSubmit(event) {
+        event.preventDefault(); //we don't want it to submit itself by default
+        
+        const { email, password } = this.state; //destructure required if state had more properties than just email and password
+
+        this.props.onSubmit({ email, password }); //this onSubmit function is passed as a prop from Login component
+    }
+
     render() {
         return (
             <div className="row">
-                <form className="col s4">
+                <form onSubmit={this.onSubmit.bind(this)}className="col s4">
                     <div className="input-field">
-                        <label>Email</label>
                         <input 
+                            placeholder="Email"
                             value={this.state.email}
                             onChange={e => this.setState({ email: e.target.value })}
                         />
                     </div>
                     <div className="input-field">
-                        <label>Password</label>
                         <input 
+                            placeholder="Password"
+                            type="password"
                             value={this.state.password}
                             onChange={e => this.setState({ password: e.target.value })}
                         />

37158cc692285d17837a311e3694c28ec5512c46
Ryan Waszak <rwaszak@tesla.com>
2021-07-01 17:22:22 -0700

    add nodemon and script to run app, add mongodb connection string

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://ryan:gQJbQjUl9hREbw8U@cluster0.iokly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;

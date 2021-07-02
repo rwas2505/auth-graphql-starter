999be68fc6ddd90a5e41d22c99978b8d334cac86
Ryan Waszak <rwaszak@tesla.com>
2021-07-02 10:20:46 -0700

    add query to get current user and load into header component to determine if user is logged in, log result, tell apollo to send cookies to backend server

--- client/index.js
+++ client/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    // this is saying its safe to send along cookies with the outgoing request
+    // it means you are making a request to the same origin that the browser is currently on
+    // it tells apollo client to send along cookies whenever it makes a query to the backend server
+    // note that this.props.data actually had the user information before I added this code, but adding it for visibility
+    credentials: 'same-origin'
+  }
+});
+
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
